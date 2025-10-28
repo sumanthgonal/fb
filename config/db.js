fb05dde1fcd23cf1fe619421125b184f59450841
sumanth gonale <sumanth.gonal@tandemloop.in>
2025-10-28 14:13:08 +0530

    SSL chnageszr

--- config/db.js
+++ config/db.js
@@ -11,6 +11,9 @@ const pool = new Pool({
   database: process.env.DB_NAME,
   password: process.env.DB_PASSWORD,
   port: process.env.DB_PORT,
+  ssl: process.env.NODE_ENV === 'production' ? {
+    rejectUnauthorized: false
+  } : false
 });
 
 // Test database connection
